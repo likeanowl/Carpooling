27a0cae24ae717678ccc4ccf31b6d715339e1017
Svitkov Sergey <sum6969@yandex.ru>
2017-01-15 07:01:42 +0300

    Even more refactorings

--- src/main/java/io/github/likeanowl/Carpooling/Agents/TravellerAgent.java
+++ src/main/java/io/github/likeanowl/Carpooling/Agents/TravellerAgent.java
@@ -13,7 +13,6 @@ import jade.lang.acl.MessageTemplate;
 import org.jgrapht.alg.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -61,9 +60,6 @@ public class TravellerAgent extends Agent {
 		}
 	}
 
-	@Override
-	protected void takeDown() {}
-
 	private void initRoadmap() {
 		roads.addVertex("Moscow");
 		roads.addVertex("SaintPetersburg");
@@ -84,53 +80,64 @@ public class TravellerAgent extends Agent {
 	private class LifeCycle extends SequentialBehaviour {
 		public LifeCycle(final Agent agent) {
 			super(agent);
-			addBehaviour(new OneShotBehaviour() {
-				@Override
-				public void action() {
-					if (cyclesCount < MAX_CYCLES_COUNT) {
-						cyclesCount++;
-						drivers = new HashSet<>();
-						coDriver = null;
-						maxEconomy = 0;
-						DFAgentDescription template = new DFAgentDescription();
-						ServiceDescription serviceDescription = new ServiceDescription();
-						serviceDescription.setType(CARPOOLING);
-						template.addServices(serviceDescription);
-						try {
-							DFAgentDescription[] result = DFService.search(agent, template);
-							for (DFAgentDescription res : result) {
-								if (!res.getName().equals(agent.getAID()))
-									drivers.add(res.getName());
-							}
-						} catch (FIPAException fe) {
-							fe.printStackTrace();
-						}
-						if (drivers.size() > 0) {
-							ParallelBehaviour parallelBehaviour = new ParallelBehaviour(agent, ParallelBehaviour.WHEN_ALL);
-							parallelBehaviour.addSubBehaviour(new DriverSearcher(agent));
-							parallelBehaviour.addSubBehaviour(new PassengerSearcher(agent));
-							addSubBehaviour(parallelBehaviour);
-							addSubBehaviour(new OfferRequestServer(agent));
-						} else {
-							addBehaviour(new WakerBehaviour(agent, TIMEOUT1) {
-								@Override
-								protected void onWake() {
-									System.out.println("restart");
-									addBehaviour(new LifeCycle(myAgent));
-								}
-							});
-						}
-					} else {
-						System.out.println(ANSI_RED + agent.getLocalName() + ": goes alone" + ANSI_RESET);
-						try {
-							DFService.deregister(agent);
-						} catch (FIPAException fe) {
-							fe.printStackTrace();
-						}
-						doDelete();
+			CycleLogic cycleLogic = new CycleLogic(agent, this);
+			addBehaviour(cycleLogic);
+		}
+	}
+
+	private class CycleLogic extends OneShotBehaviour {
+		private Agent agent;
+		private LifeCycle cycle;
+
+		public CycleLogic(Agent agent, LifeCycle cycle) {
+			this.agent = agent;
+			this.cycle = cycle;
+		}
+
+		@Override
+		public void action() {
+			if (cyclesCount < MAX_CYCLES_COUNT) {
+				cyclesCount++;
+				drivers = new HashSet<>();
+				coDriver = null;
+				maxEconomy = 0;
+				DFAgentDescription template = new DFAgentDescription();
+				ServiceDescription serviceDescription = new ServiceDescription();
+				serviceDescription.setType(CARPOOLING);
+				template.addServices(serviceDescription);
+				try {
+					DFAgentDescription[] result = DFService.search(agent, template);
+					for (DFAgentDescription res : result) {
+						if (!res.getName().equals(agent.getAID()))
+							drivers.add(res.getName());
 					}
+				} catch (FIPAException fe) {
+					fe.printStackTrace();
 				}
-			});
+				if (drivers.size() > 0) {
+					ParallelBehaviour parallelBehaviour = new ParallelBehaviour(agent, ParallelBehaviour.WHEN_ALL);
+					parallelBehaviour.addSubBehaviour(new DriverSearcher(agent));
+					parallelBehaviour.addSubBehaviour(new PassengerSearcher(agent));
+					cycle.addSubBehaviour(parallelBehaviour);
+					cycle.addSubBehaviour(new OfferRequestServer(agent));
+				} else {
+					addBehaviour(new WakerBehaviour(agent, TIMEOUT1) {
+						@Override
+						protected void onWake() {
+							System.out.println("restart");
+							addBehaviour(new LifeCycle(myAgent));
+						}
+					});
+				}
+			} else {
+				System.out.println(ANSI_RED + agent.getLocalName() + ": goes alone" + ANSI_RESET);
+				try {
+					DFService.deregister(agent);
+				} catch (FIPAException fe) {
+					fe.printStackTrace();
+				}
+				doDelete();
+			}
 		}
 	}
 
@@ -139,61 +146,82 @@ public class TravellerAgent extends Agent {
 
 		public DriverSearcher(Agent agent) {
 			super(agent);
-			addSubBehaviour(new ProposeSender(agent)); // send proposals for drivers
+			ProposeSender proposeSender = new ProposeSender(agent);
+			addSubBehaviour(proposeSender); // send proposals for drivers
+			replyWith = proposeSender.getReplyWith();
 			ParallelBehaviour parallelBehaviour = new ParallelBehaviour(agent, ParallelBehaviour.WHEN_ALL);
 			for (AID dr : drivers) {
-				parallelBehaviour.addSubBehaviour(new ProposeReplyReceiver(agent, dr)); //add Receiver for all proposals
+				parallelBehaviour.addSubBehaviour(new ProposeReplyReceiver(agent, dr, replyWith)); //add Receiver for all
+				// proposals
 			}
 			addSubBehaviour(parallelBehaviour); // receive replies from drivers
 		}
+	}
 
-		private class ProposeSender extends OneShotBehaviour {
-			public ProposeSender(Agent agent) {
-				super(agent);
-				replyWith = System.currentTimeMillis() + "";
-			}
+	private class ProposeSender extends OneShotBehaviour {
+		private String replyWith;
 
-			public void action() {
-				ACLMessage msg = new ACLMessage(ACLMessage.CFP);
-				msg.removeReceiver(getAID());
-				msg.setContent(currentPlace + " " + targetPlace + " " + distance);
-				msg.setConversationId(CARPOOLING);
-				msg.setReplyWith(replyWith);
-				System.out.println(myAgent.getLocalName() + ": call for proposals");
-				for (AID dr : drivers) {
-					msg.addReceiver(dr);
-				}
-				myAgent.send(msg);
-			}
+		public ProposeSender(Agent agent) {
+			super(agent);
+			replyWith = String.valueOf(System.currentTimeMillis());
 		}
 
-		private class ProposeReplyReceiver extends SequentialBehaviour {
-			public ProposeReplyReceiver(final Agent agent, final AID sender) {
-				final Handle handle = ReceiverBehaviour.newHandle();
-				addSubBehaviour(new ReceiverBehaviour(agent, handle, MILLIS, MessageTemplate.and(MessageTemplate.MatchSender(sender),
-						MessageTemplate.and(MessageTemplate.MatchConversationId(CARPOOLING), MessageTemplate.MatchInReplyTo(replyWith)))));
-				addSubBehaviour(new OneShotBehaviour(agent) {
-					@Override
-					public void action() {
-						try {
-							ACLMessage msg = handle.getMessage();
-							if (msg.getPerformative() == ACLMessage.PROPOSE) {
-								int price = Integer.parseInt(msg.getContent());
-								if (distance - price > maxEconomy) {
-									maxEconomy = distance - price;
-									coDriver = msg.getSender();
-									travelerCategory = PASSENGER;
-								}
-							}
-						} catch (ReceiverBehaviour.TimedOut timedOut) {
-							System.out.println(agent.getLocalName() + ": time out while receiving message from " + sender
-									.getLocalName());
-						} catch (ReceiverBehaviour.NotYetReady notYetReady) {
-							System.out.println(agent.getLocalName() + ": message from " + sender.getLocalName() + " not yet " +
-									"ready");
-						}
+		public void action() {
+			ACLMessage msg = new ACLMessage(ACLMessage.CFP);
+			msg.removeReceiver(getAID());
+			msg.setContent(currentPlace + " " + targetPlace + " " + distance);
+			msg.setConversationId(CARPOOLING);
+			msg.setReplyWith(replyWith);
+			System.out.println(myAgent.getLocalName() + ": call for proposals");
+			for (AID dr : drivers) {
+				msg.addReceiver(dr);
+			}
+			myAgent.send(msg);
+		}
+
+		public String getReplyWith() {
+			return replyWith;
+		}
+	}
+
+	private class ProposeReplyReceiver extends SequentialBehaviour {
+		public ProposeReplyReceiver(final Agent agent, final AID sender, String replyWith) {
+			final Handle handle = ReceiverBehaviour.newHandle();
+			addSubBehaviour(new ReceiverBehaviour(agent, handle, MILLIS, MessageTemplate.and(MessageTemplate.MatchSender(sender),
+					MessageTemplate.and(MessageTemplate.MatchConversationId(CARPOOLING), MessageTemplate.MatchInReplyTo(replyWith)))));
+			addSubBehaviour(new CategoryReplier(agent, handle, sender));
+		}
+	}
+
+	private class CategoryReplier extends OneShotBehaviour {
+		private Handle handle;
+		private Agent agent;
+		private AID sender;
+
+		public CategoryReplier(Agent agent, Handle handle, AID sender) {
+			this.agent = agent;
+			this.handle = handle;
+			this.sender = sender;
+		}
+
+		@Override
+		public void action() {
+			try {
+				ACLMessage msg = handle.getMessage();
+				if (msg.getPerformative() == ACLMessage.PROPOSE) {
+					int price = Integer.parseInt(msg.getContent());
+					if (distance - price > maxEconomy) {
+						maxEconomy = distance - price;
+						coDriver = msg.getSender();
+						travelerCategory = PASSENGER;
 					}
-				});
+				}
+			} catch (ReceiverBehaviour.TimedOut timedOut) {
+				System.out.println(agent.getLocalName() + ": time out while receiving message from " + sender
+						.getLocalName());
+			} catch (ReceiverBehaviour.NotYetReady notYetReady) {
+				System.out.println(agent.getLocalName() + ": message from " + sender.getLocalName() + " not yet " +
+						"ready");
 			}
 		}
 	}
@@ -206,49 +234,62 @@ public class TravellerAgent extends Agent {
 			}
 			//Reply to all proposals
 		}
+	}
 
-		private class ProposeReplier extends SequentialBehaviour {
-			public ProposeReplier(final Agent agent, final AID sender) {
-				final Handle handle = ReceiverBehaviour.newHandle();
-				addSubBehaviour(new ReceiverBehaviour(agent, handle, MILLIS1
-						, MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.CFP)
-						, MessageTemplate.MatchSender(sender))));
-				addSubBehaviour(new OneShotBehaviour(agent) {
-					@Override
-					public void action() {
-						try {
-							ACLMessage msg = handle.getMessage();
-							ACLMessage reply = msg.createReply();
-							String[] content = msg.getContent().split(" ");
-							int passengersDistance = Integer.parseInt(content[2]);
-							if (passengersDistance <= distance) {
-								int extraDistance = ((int) new DijkstraShortestPath<>(roads, currentPlace, content[0]).getPathLength() +
-										(int) new DijkstraShortestPath<>(roads, content[1], targetPlace).getPathLength() + passengersDistance) - distance;
-								int price = (int) (0.5 * (extraDistance + passengersDistance));
-								if (0.5 * (passengersDistance - extraDistance) > maxEconomy) {
-									maxEconomy = (int) (0.5 * (passengersDistance - extraDistance));
-									coDriver = msg.getSender();
-									travelerCategory = DRIVER;
-									if (passengersDistance == distance)
-										travelerCategory = DRIVER + " or " + PASSENGER;
-								}
-								reply.setPerformative(ACLMessage.PROPOSE);
-								System.out.println(myAgent.getLocalName() + ": send propose to "
-										+ msg.getSender().getLocalName() + "; price: " + price);
-								reply.setContent(Integer.toString(price));
-							} else {
-								reply.setPerformative(ACLMessage.DISCONFIRM);
-								System.out.println(myAgent.getLocalName() + ": way to " + msg.getSender().getLocalName()
-										+ " is too long");
-							}
-							myAgent.send(reply);
-						} catch (ReceiverBehaviour.TimedOut timedOut) {
-							System.out.println(agent + ": time out while receiving cfp from " + sender.getLocalName());
-						} catch (ReceiverBehaviour.NotYetReady notYetReady) {
-							System.out.println(agent + ": cfp from " + sender.getLocalName() + " not yet ready");
-						}
+	private class ProposeReplier extends SequentialBehaviour {
+		public ProposeReplier(final Agent agent, final AID sender) {
+			final Handle handle = ReceiverBehaviour.newHandle();
+			addSubBehaviour(new ReceiverBehaviour(agent, handle, MILLIS1
+					, MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.CFP)
+					, MessageTemplate.MatchSender(sender))));
+			ProposeLogic proposeLogic = new ProposeLogic(handle, agent, sender);
+			addSubBehaviour(proposeLogic);
+		}
+	}
+
+	private class ProposeLogic extends OneShotBehaviour {
+		private Handle handle;
+		private AID sender;
+		private Agent agent;
+
+		public ProposeLogic(Handle handle, Agent agent, AID sender) {
+			this.handle = handle;
+			this.agent = agent;
+			this.sender = sender;
+		}
+
+		@Override
+		public void action() {
+			try {
+				ACLMessage msg = handle.getMessage();
+				ACLMessage reply = msg.createReply();
+				String[] content = msg.getContent().split(" ");
+				int passengersDistance = Integer.parseInt(content[2]);
+				if (passengersDistance <= distance) {
+					int extraDistance = ((int) new DijkstraShortestPath<>(roads, currentPlace, content[0]).getPathLength() +
+							(int) new DijkstraShortestPath<>(roads, content[1], targetPlace).getPathLength() + passengersDistance) - distance;
+					int price = (int) (0.5 * (extraDistance + passengersDistance));
+					if (0.5 * (passengersDistance - extraDistance) > maxEconomy) {
+						maxEconomy = (int) (0.5 * (passengersDistance - extraDistance));
+						coDriver = msg.getSender();
+						travelerCategory = DRIVER;
+						if (passengersDistance == distance)
+							travelerCategory = DRIVER + " or " + PASSENGER;
 					}
-				});
+					reply.setPerformative(ACLMessage.PROPOSE);
+					System.out.println(myAgent.getLocalName() + ": send propose to "
+							+ msg.getSender().getLocalName() + "; price: " + price);
+					reply.setContent(Integer.toString(price));
+				} else {
+					reply.setPerformative(ACLMessage.DISCONFIRM);
+					System.out.println(myAgent.getLocalName() + ": way to " + msg.getSender().getLocalName()
+							+ " is too long");
+				}
+				myAgent.send(reply);
+			} catch (ReceiverBehaviour.TimedOut timedOut) {
+				System.out.println(agent + ": time out while receiving cfp from " + sender.getLocalName());
+			} catch (ReceiverBehaviour.NotYetReady notYetReady) {
+				System.out.println(agent + ": cfp from " + sender.getLocalName() + " not yet ready");
 			}
 		}
 	}
@@ -270,85 +311,34 @@ public class TravellerAgent extends Agent {
 				sequentialBehaviour.addSubBehaviour(new ReceiverBehaviour(myAgent, handle, MILLIS2
 						, MessageTemplate.and(MessageTemplate.MatchConversationId(AGREE_FOR_CARPOOLING)
 						, MessageTemplate.MatchSender(coDriver))));
-				DecisionBehaviour decisionBehaviour = new DecisionBehaviour(myAgent, handle, coDriverName);
-				sequentialBehaviour.addSubBehaviour(decisionBehaviour);
-				/*sequentialBehaviour.addSubBehaviour(new OneShotBehaviour(myAgent) {
-					@Override
-					public void action() {
-						try {
-							ACLMessage msg = handle.getMessage();
-							if (msg.getPerformative() == ACLMessage.AGREE) {
-								System.out.println(ANSI_RED + myAgent.getLocalName() + ": goes with "
-										+ coDriverName + " as " + travelerCategory + ANSI_RESET);
-								ParallelBehaviour offer = new ParallelBehaviour(myAgent, 2);
-								offer.addSubBehaviour(new Refuser(myAgent));
-								offer.addSubBehaviour(new OneShotBehaviour(myAgent) {
-									@Override
-									public void action() {
-										try {
-											DFService.deregister(myAgent);
-										} catch (FIPAException fe) {
-											fe.printStackTrace();
-										}
-									}
-								});
-								offer.addSubBehaviour(new WakerBehaviour(myAgent, 5000) {
-									@Override
-									protected void onWake() {
-										doDelete();
-									}
-								});
-								addBehaviour(offer);
-							} else {
-								addBehaviour(new WakerBehaviour(myAgent, 5000) {
-									@Override
-									protected void onWake() {
-										System.out.println(myAgent.getLocalName() + ": restart life cycle");
-										addBehaviour(new LifeCycle(myAgent));
-									}
-								});
-							}
-						} catch (ReceiverBehaviour.NotYetReady notYetReady) {
-							System.out.println(myAgent.getLocalName() + ": reply not yet ready");
-							addBehaviour(new WakerBehaviour(myAgent, 3000) {
-								@Override
-								protected void onWake() {
-									System.out.println(myAgent.getLocalName() + ": restart life cycle");
-									addBehaviour(new LifeCycle(myAgent));
-								}
-							});
-						} catch (ReceiverBehaviour.TimedOut timedOut) {
-							System.out.println(myAgent.getLocalName() + ": time out");
-							addBehaviour(new WakerBehaviour(myAgent, 3000) {
-								@Override
-								protected void onWake() {
-									System.out.println(myAgent.getLocalName() + ": restart life cycle");
-									addBehaviour(new LifeCycle(myAgent));
-								}
-							});
-						}
-					}
-				});*/
-				parallelBehaviour.addSubBehaviour(sequentialBehaviour);                  //receive AGREE or REFUSE from coDriver
+				DriverDecision driverDecision = new DriverDecision(myAgent, handle, coDriverName);
+				sequentialBehaviour.addSubBehaviour(driverDecision);
+				parallelBehaviour.addSubBehaviour(sequentialBehaviour);//receive AGREE or REFUSE from coDriver
 				parallelBehaviour.addSubBehaviour(new Refuser(myAgent));//REFUSE offers wrom other travelers
 				addSubBehaviour(parallelBehaviour);
 			} else {
-				addBehaviour(new WakerBehaviour(myAgent, 5000) {
-					@Override
-					protected void onWake() {
-						System.out.println(myAgent.getLocalName() + ": restart life cycle");
-						addBehaviour(new LifeCycle(myAgent));
-					}
-				});
+				addBehaviour(new Restarter(myAgent, 5000));
 			}
 		}
 	}
 
-	private class DecisionBehaviour extends OneShotBehaviour {
+	private class Restarter extends WakerBehaviour {
+		public Restarter(Agent a, long timeout) {
+			super(a, timeout);
+		}
+
+		@Override
+		protected void onWake() {
+			System.out.println(myAgent.getLocalName() + ": restart life cycle");
+			addBehaviour(new LifeCycle(myAgent));
+		}
+	}
+
+	private class DriverDecision extends OneShotBehaviour {
 		private Handle handle;
 		private String coDriverName;
 
-		public DecisionBehaviour(Agent agent, Handle handle, String coDriverName) {
+		public DriverDecision(Agent agent, Handle handle, String coDriverName) {
 			super(agent);
 			this.handle = handle;
 			this.coDriverName = coDriverName;
@@ -449,4 +439,3 @@ public class TravellerAgent extends Agent {
 		}
 	}
 }
-
